708e8a20bd01f7389cba0c510c86ac890d0c43b5
JPumpkin645 <jpark9595@naver.com>
2024-02-16 01:13:54 +0900

    Feat: Add bottom navigation bar with icons and text
    - Added a new feature to the main screen that includes a bottom navigation bar.
    - Each tab in the navigation bar has an icon and text.
    - The selected tab is highlighted with increased opacity.
    - When a tab is tapped, the corresponding screen is displayed.
    - Implemented functionality to increase a counter when a button is pressed on one of the screens.

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -11,20 +11,7 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   int _selectedIndex = 0;
-  final screens = [
-    const Center(
-      child: Text(
-        'Home',
-        style: TextStyle(fontSize: 49),
-      ),
-    ),
-    const Center(
-      child: Text(
-        'Search',
-        style: TextStyle(fontSize: 49),
-      ),
-    ),
-  ];
+
   void _onTap(int index) {
     setState(() {
       _selectedIndex = index;
@@ -34,6 +21,26 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Stack(
+        children: [
+          Offstage(
+            offstage: _selectedIndex != 0,
+            child: const StfScreen(),
+          ),
+          Offstage(
+            offstage: _selectedIndex != 1,
+            child: const StfScreen(),
+          ),
+          Offstage(
+            offstage: _selectedIndex != 3,
+            child: const StfScreen(),
+          ),
+          Offstage(
+            offstage: _selectedIndex != 4,
+            child: const StfScreen(),
+          )
+        ],
+      ),
       bottomNavigationBar: BottomAppBar(
         color: Colors.black,
         child: Padding(
@@ -45,29 +52,74 @@ class _MainScreenState extends State<MainScreen> {
                 text: "Home",
                 isSelected: _selectedIndex == 0,
                 icon: FontAwesomeIcons.house,
+                selectedIcon: FontAwesomeIcons.house,
                 onTap: () => _onTap(0),
               ),
               NavTab(
                 text: "Discover",
                 isSelected: _selectedIndex == 1,
-                icon: FontAwesomeIcons.magnifyingGlass,
+                icon: FontAwesomeIcons.compass,
+                selectedIcon: FontAwesomeIcons.solidCompass,
                 onTap: () => _onTap(1),
               ),
               NavTab(
                 text: "Inbox",
                 isSelected: _selectedIndex == 3,
                 icon: FontAwesomeIcons.message,
+                selectedIcon: FontAwesomeIcons.solidMessage,
                 onTap: () => _onTap(3),
               ),
               NavTab(
                 text: "Profile",
                 isSelected: _selectedIndex == 4,
                 icon: FontAwesomeIcons.user,
+                selectedIcon: FontAwesomeIcons.solidUser,
                 onTap: () => _onTap(4),
               ),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class StfScreen extends StatefulWidget {
+  const StfScreen({super.key});
+  @override
+  State<StfScreen> createState() => _StfScreenState();
+}
+
+class _StfScreenState extends State<StfScreen> {
+  int _clicks = 0;
+  void _increase() {
+    setState(() {
+      _clicks = _clicks + 1;
+    });
+  }
+
+  @override
+  void dispose() {
+    print(_clicks);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('im built!');
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "$_clicks",
+            style: const TextStyle(fontSize: 48),
+          ),
+          TextButton(
+            onPressed: _increase,
+            child: const Text('+'),
+          )
+        ],
       ),
     );
   }
