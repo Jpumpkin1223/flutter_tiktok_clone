7006eb86085ee8b82fcfdcb43efbac56be20009d
JPumpkin1223 <jpark9595@naver.com>
2024-04-12 15:39:37 +0900

    Feat: Add video post index and visibility detector for pause/play toggle. - Add index parameter to video post widget - Implement visibility detector for video play/pause toggle - Update video post UI to handle play/pause functionality

--- lib/screens/video_timeline_screen.dart
+++ lib/screens/video_timeline_screen.dart
@@ -11,7 +11,6 @@ class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
   int _itemCount = 4;
 
   final PageController _pageController = PageController();
-
   final Duration _scrollDuration = const Duration(milliseconds: 250);
   final Curve _scrollCurve = Curves.linear;
 
@@ -47,7 +46,7 @@ class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
       scrollDirection: Axis.vertical,
       onPageChanged: _onPageChanged,
       itemCount: _itemCount,
-      itemBuilder: (context, index) => VideoPost(onVideoFinished: _onVideoFinished),
+      itemBuilder: (context, index) => VideoPost(onVideoFinished: _onVideoFinished, index: index),
     );
   }
 }
